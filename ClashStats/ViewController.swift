f8f1d07cb64899130fa467ad24b87fe01bd80f4a
JonathasLopes <jon.athas@hotmail.com>
2022-11-15 16:00:58 -0300

    Initial Commit

---
+++ ClashStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClashStats
+//
+//  Created by Jonathas Lopes on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
